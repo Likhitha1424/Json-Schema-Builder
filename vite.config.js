b5262e548b469a96c9d053254aaa4c276a202734
Likhitha <likhithagsetty@gmail.com>
2025-07-21 19:57:03 +0530

    Deployment setup Done

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/Json-Schema-Builder/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
